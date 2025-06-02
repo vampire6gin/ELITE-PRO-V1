546f166edd004d5ee3b495a42af0d2c0f6616ca8
vampire6gin <empireududu@gmail.com>
2025-06-02 22:18:32 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 // Contact details
-global.ownernomer = process.env.OWNER_NOMER || "2348109263390";
+global.ownernomer = process.env.OWNER_NOMER || "255696761724";
 global.ytname = process.env.YT_NAME || "YT: @EliteProTech";
 global.socialm = process.env.SOCIAL_M || "GitHub: EliteProTech";
 global.location = process.env.LOCATION || "Nigeria, Port Harcourt";
 
 // Creator details
-global.ownernumber = process.env.OWNER_NUMBER || '2348109263390';
+global.ownernumber = process.env.OWNER_NUMBER || '255696 761 724';
 global.ownername = process.env.OWNER_NAME || 'ElitePro';
 global.botname = process.env.BOT_NAME || 'ELITE-PRO-V1';
 
